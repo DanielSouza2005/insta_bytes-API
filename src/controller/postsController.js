c5fa819c11fbb1ef7dd80dd59707a0de6a1a0422
DanielSouza2005 <danisimoes741@gmail.com>
2024-11-24 17:05:23 -0300

    update and delete posts feature, gemini AI integration and deploy

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -1,11 +1,101 @@
-import getPosts  from "../models/postsModel.js";
+import Posts  from "../models/postsModel.js";
+import gerarDescricaoComGemini from "../services/geminiService.js";
+import fs from "fs";
 
 export async function listarTodosPosts(req, res){
-    const posts = await getPosts.getTodosPosts();
-    res.status(200).json(posts);
+    try {
+        const posts = await Posts.getTodosPosts();
+        res.status(200).json(posts);
+    } catch (erro){
+        console.error(erro.message);
+        res.status(500).json({"Erro": "Falha na Requisição"});
+    }    
 }
 
 export async function listarPostPorId(req, res){
-    const posts = await getPosts.getPostPorID(req.params.id);
-    res.status(200).json(posts);
+    try {
+        const posts = await Posts.getPostPorID(req.params.id);
+        res.status(200).json(posts);
+    } catch (erro){
+        console.error(erro.message);
+        res.status(500).json({"Erro": "Falha na Requisição"});
+    }    
+}
+
+export async function postarNovoPost(req, res){
+    const novoPost = req.body;
+    try {
+        const postCriado = await Posts.criarNovoPost(novoPost);
+        res.status(200).json(postCriado);
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro": "Falha na Requisição"});
+    }
+}
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };    
+
+    try {
+        const postCriado = await Posts.criarNovoPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+
+        fs.renameSync(req.file.path, imagemAtualizada);
+
+        res.status(200).json(postCriado);
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro": "Falha na Requisição"});
+    }
+}
+
+export async function atualizarPost(req, res){
+    const id = req.params.id;
+    const urlImagem = `http://localhost:3000/${id}.png`;  
+    const promptDescricao = "Gere apenas a Descrição em português do Brasil para a seguinte imagem, sem parecer que está conversando com outra pessoa.";
+    const promptAlt = "Gere apenas a Alt Text em português do Brasil para a seguinte imagem, sem parecer que está conversando com outra pessoa.";
+
+    try {
+        const imageBuffer = fs.readFileSync(`uploads/${id}.png`); 
+        const descricao = await gerarDescricaoComGemini(imageBuffer, promptDescricao);
+        const alt = await gerarDescricaoComGemini(imageBuffer, promptAlt);
+        
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricao, //req.body.descricao
+            alt: alt //req.body.alt
+        }
+
+        const postCriado = await Posts.atualizarNovoPost(id, post);
+
+        res.status(200).json(postCriado);
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro": "Falha na Requisição"});
+    }    
+}
+
+export async function apagarPost(req, res){
+    try {        
+        const urlImagem = `uploads/${req.params.id}.png`;  
+
+        try {
+            fs.unlinkSync(urlImagem);
+        } catch (erro) { 
+            console.error(erro.message);
+            res.status(500).json({"Erro": "Falha na Requisição"});
+            return;
+        }
+
+        const post = await Posts.apagarPost(req.params.id);
+
+        res.status(200).json(post);
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro": "Falha na Requisição"});
+    }
 }
--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import conectaBanco from "../config/dbConfig.js";
 import { ObjectId } from "mongodb";
 const conexao = await conectaBanco(process.env.STRING_CONEXAO);
@@ -5,14 +6,39 @@ const conexao = await conectaBanco(process.env.STRING_CONEXAO);
 async function getTodosPosts(){
     const db = conexao.db("instabyte");
     const colecao = db.collection("posts");
+
     return colecao.find().toArray();
 };
 
 async function getPostPorID(id) {
     const db = conexao.db("instabyte");
-    const colecao = db.collection("posts"); 
+    const colecao = db.collection("posts");     
     const document = await colecao.findOne({ _id: ObjectId.createFromHexString(id) }); 
+
     return document;
 }
 
-export default { getTodosPosts, getPostPorID };
+async function criarNovoPost(post){
+    const db = conexao.db("instabyte");
+    const colecao = db.collection("posts");
+
+    return colecao.insertOne(post);
+}
+
+async function atualizarNovoPost(id, post){
+    const db = conexao.db("instabyte");
+    const colecao = db.collection("posts");
+    const objID = ObjectId.createFromHexString(id);
+
+    return colecao.updateOne({_id: new ObjectId(objID)}, {$set: post});        
+}
+
+async function apagarPost(id){
+    const db = conexao.db("instabyte");
+    const colecao = db.collection("posts");
+    const objID = ObjectId.createFromHexString(id);
+
+    return colecao.deleteOne({_id: new ObjectId(objID)});
+}
+
+export default { getTodosPosts, getPostPorID, criarNovoPost, atualizarNovoPost, apagarPost };
